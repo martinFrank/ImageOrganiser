a21b254229e5bc7752815c3dcf6783cbc702c442
MartinFrank77 <martin.frank@irix.ch>
2025-10-16 17:17:16 +0200

    added backend

---
+++ backend/imageorganiser/src/main/java/com/github/martinfrank/imageorganiser/imageorganiser/ImageorganiserApplication.java
@@ -0,0 +1,12 @@
+package com.github.martinfrank.imageorganiser.imageorganiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageorganiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageorganiserApplication.class, args);
+    }
+}
---
+++ backend/imageorganiser/src/test/java/com/github/martinfrank/imageorganiser/imageorganiser/ImageorganiserApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.martinfrank.imageorganiser.imageorganiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageorganiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
